0119f4b1669a66321761b0da72abb557c3ae2114
user <kimjaehyuk1208@gmail.com>
2025-04-01 11:48:04 +0900

    파이 구하기 와 그래프 그리기

---
+++ p01pi.py
@@ -0,0 +1,26 @@
+# 오일러 공식을 이용한 파이 근사값 구하기
+
+p = 1
+
+n = 1
+p = p * ((2*n + 1)**2 - 1 )/ (2*n + 1) **2
+
+n = 2
+p = p * ((2 * n + 1)**2 - 1 )/ (2 * n + 1) **2
+
+print(p*4)
+
+
+# 루프의 변환
+
+p =1
+pilist =[]
+for n in range(1,10000):
+    p =  p * ((2* n +1)** 2 -1 ) / (2 * n + 1 )** 2
+    # print(p*4,',')
+    pilist.append(p*4)
+
+import  matplotlib.pyplot as plt
+plt.plot(pilist)
+plt.show()
+
